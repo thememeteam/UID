a29440643bd32e3d87c0c2a337e8cbff6751448e
J S Vaibhav <151757419+Agerarsr@users.noreply.github.com>
2024-04-19 14:27:38 +0530

    Models 2

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -1,11 +1,16 @@
+import Article from "@/components/Article";
+
 const Models: React.FC = () => {
 	return (
 		<div className="w-full h-full grid grid-rows-[repeat(3,30rem)]">
 			<div className="bg-red-500">
 				<h1>Vitesse</h1>
 			</div>
-			<div className="bg-green-500 flex flex-" >
-				<h1 className="font-display text-h1 accentline">R6</h1>
+			<div className="bg-green-500 flex flex-col justify-center pl-6" >
+				<img src="porsche-normal.jpg"></img>
+				<Article title="R6" className="ml-32">
+				<p>track performance, road practicality</p>
+				</Article>
 			</div>
 			<div className="bg-blue-500" >
 				<h1>Nuclide</h1>
