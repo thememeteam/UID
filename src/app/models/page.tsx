f7a6290f44aa1e3a5130876357d62cd4fe4a1b4a
J S Vaibhav <151757419+Agerarsr@users.noreply.github.com>
2024-04-19 16:39:53 +0530

    models page without gradient

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -2,12 +2,15 @@ import Article from "@/components/Article";
 import Link from "next/link";
 
 const Models: React.FC = () => {
-	return (
-		<div className="w-full h-full grid grid-rows-[repeat(3,30rem)]">
-			<div className="bg-red-500">
-				<h1>Vitesse</h1>
+	return (	
+		<div className="w-full h-full grid grid-rows-[repeat(3,32rem)] ">
+			<div className="bg-[url('/gtc4lusso.png')] bg-cover bg-bottom flex flex-col justify-center p-6 ">
+				<Article title="Vitesse" className="self-end mr-32">
+				<p>Luxury for the week, performance for the weekend</p>
+				<Link href="/models/nuclide" className="bg-accent text-black p-2">Learn more</Link>
+				</Article>
 			</div>
-			<div className="bg-[url('/porsche-normal.jpg')] bg-cover bg-center relative flex flex-col justify-center p-6" >
+			<div className="bg-[url('/porsche-normal.jpg')] bg-cover relative flex flex-col justify-center p-6" >
 				{/* <img className="absolute top-0 left-0 w-full object-cover" src="porsche-normal.jpg"/> */}
 				<Article title="R6" className="ml-32">
 				<p>Track performance, road practicality</p>
@@ -15,7 +18,7 @@ const Models: React.FC = () => {
 				</Article>
 			</div>
 			<div className="bg-[url('/P1.webp')] bg-cover bg-center relative flex flex-col justify-center p-6" >
-			<Article title="Nuclide" className="self-end mr-32">
+				<Article title="Nuclide" className="self-end mr-32">
 				<p>The ultimate track weapon.<br/><del>May</del> Will spontaneously release energy.</p>
 				<Link href="/models/nuclide" className="bg-accent text-black p-2">Learn more</Link>
 				</Article>
