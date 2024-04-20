4b1dd6ec62867fa6e08a6fca1e1430a19cca27c0
Rishblol <rish90093@gmail.com>
2024-04-20 19:57:10 +0530

    edited vaibhav's adoption papers

--- src/app/network/page.tsx
+++ src/app/network/page.tsx
@@ -3,7 +3,7 @@ const Network: React.FC = () => {
 		<>
 			<div className="flex w-full h-full justify-center items-center">
 				<iframe title="maps"
-					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14129.91530047874!2d77.63541401918708!3d12.893868107442847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae15ccb7f88029%3A0xa2827e1a3626894d!2sLuxe%20Cars%20by%20Automatch!5e1!3m2!1sen!2sin!4v1713288877280!5m2!1sen!2sin"
+					src= "https://www.google.com/maps/d/embed?mid=16b1wRMebZgQuMdUbbN1wZM59MIZAMTk&ehbc=2E312F&noprof=1"
 					width="800"
 					height="500"
 				/>
