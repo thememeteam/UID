aca50ed26fd0bcf1526472c6c24c47772f016e04
Rishblol <rish90093@gmail.com>
2024-04-21 21:18:37 +0530

    edited Vaibhav's brain size

--- src/app/network/page.tsx
+++ src/app/network/page.tsx
@@ -1,11 +1,11 @@
 const Network: React.FC = () => {
 	return (
 		<>
-			<div className="flex w-full h-full justify-center items-center">
+			<div className="flex w-full h-full justify-center items-center mt-8">
 				<iframe title="maps"
 					src= "https://www.google.com/maps/d/embed?mid=16b1wRMebZgQuMdUbbN1wZM59MIZAMTk&ehbc=2E312F&noprof=1"
 					width="800"
-					height="500"
+					height="550"
 				/>
 			</div>
 		</>
