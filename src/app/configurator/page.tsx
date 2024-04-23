456d8fa63e14859d1af3155a1dad39683a4e878d
Rishblol <rish90093@gmail.com>
2024-04-23 15:59:26 +0530

    nuclide model

--- src/app/configurator/page.tsx
+++ src/app/configurator/page.tsx
@@ -1,23 +1,28 @@
 "use client";
-
 import { Canvas, extend } from "@react-three/fiber";
-import { PresentationControls } from "@react-three/drei";
-import { Mesh, MeshNormalMaterial,} from "three";
-extend({ Canvas, Mesh, MeshNormalMaterial });
+import { PerspectiveCamera, PresentationControls } from "@react-three/drei";
+import { Mesh, MeshNormalMaterial, } from "three";
+import { Nuclide } from "./Nuclide";
+import Spacer from "@/components/Spacer";
+// extend({ Canvas, Mesh, MeshNormalMaterial });
+
 const Configurator: React.FC = () => {
 	return (
-		<>
-		<div style={{ width: "100%", height: "100%" }}>
-			<Canvas className="beans">
-				<PresentationControls>
-					<mesh>
-						<boxGeometry/>
-						<meshNormalMaterial />
-					</mesh>
-				</PresentationControls>
-			</Canvas>
+		<div className="w-full h-full">
+			<Spacer />
+			<div style={{ width: "100%", height: "100vh" }} className="items-center align-middle">
+				<Canvas camera={{ fov: 40 }}>
+					<PresentationControls global zoom={1} polar={[-0.1, Math.PI / 4]}>
+						<mesh>
+							<ambientLight intensity={1} />
+							<directionalLight color="white" position={[0, 5, 8]} castShadow={true} />
+							<Nuclide />
+
+						</mesh>
+					</PresentationControls>
+				</Canvas>
+			</div>
 		</div>
-		</>
 	);
 };
 
