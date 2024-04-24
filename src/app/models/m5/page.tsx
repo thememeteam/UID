81ac4d7ccc2665ad6688bcb97c105135626e4c49
J S Vaibhav <151757419+Agerarsr@users.noreply.github.com>
2024-04-24 14:43:58 +0530

    remove

--- src/app/models/m5/page.tsx
+++ src/app/models/m5/page.tsx
@@ -4,23 +4,6 @@ const M5Page: React.FC = () => {
     return (
         
 		<>
-        {/* <div className="relative max-h-screen">
-            <div
-                className="absolute w-full h-full flex flex-col items-center justify-center gap-2"
-                style={{
-                    background:
-                        "linear-gradient(to bottom, var(--background-fill) 0%, transparent 40%, var(--background-fill) 100%",
-                }}
-            >
-                <p className="text-6xl font-display mix-blend-difference text-orange-400">
-                    Feynman M5
-                </p>
-            </div>
-            <img
-                src="/assets/assets/m5/m5model1.jpg"
-                className="max-h-[38rem] w-screen object-cover"
-            />
-        </div> */}
         <Hero type="video" src="/assets/assets/m5/M5video (2).mp4" alt="showcase of a BMW M5">
             <span className=" mix-blend-difference nomargin text-8xl font-display text-accent">
                 Feynman M5
