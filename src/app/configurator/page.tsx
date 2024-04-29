1b309d2e622abf8ebf6fed99fd5cd74a45e685f5
heftymouse <59918974+heftymouse@users.noreply.github.com>
2024-04-29 08:57:28 +0530

    add terrible segmented control

--- src/app/configurator/page.tsx
+++ src/app/configurator/page.tsx
@@ -3,10 +3,16 @@ import { Canvas, extend } from "@react-three/fiber";
 import { AccumulativeShadows, CubeCamera, MeshReflectorMaterial, OrthographicCamera, PerspectiveCamera, PresentationControls, RandomizedLight, Shadow, SpotLight, SpotLightShadow, Stage } from "@react-three/drei";
 import { Camera, Mesh, MeshNormalMaterial, } from "three";
 import { Nuclide } from "./Nuclide";
+import Spacer from "@/components/Spacer";
+import Segmented from "@/components/Segmented";
+
 const Configurator: React.FC = () => {
 	return (
 		<>
-			<div className="w-full h-fit">
+			<Spacer />
+			<Segmented options={['M5', 'Nuclide', 'Viper']} onChange={(e) => alert(e)} />
+
+			{/* <div className="w-full h-fit">
 				<div style={{ width: "100%", height: "100vh " }} className="items-center align-middle justify-between">
 					<Canvas camera={{fov:30}} shadows>
 						<PresentationControls global zoom={1.2} polar={[0.1, Math.PI / 30]}>
@@ -17,7 +23,7 @@ const Configurator: React.FC = () => {
 						</PresentationControls>
 					</Canvas>
 				</div>
-			</div>
+			</div> */}
 		</>
 	);
 };
