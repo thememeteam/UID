70d30c9114ac182e7b2c69e9972b7227bb9b17c4
Rishblol <rish90093@gmail.com>
2024-04-30 22:46:10 +0530

    edited configurator

--- src/app/configurator/page.tsx
+++ src/app/configurator/page.tsx
@@ -5,29 +5,23 @@ import { Camera, ColorManagement, Mesh, MeshNormalMaterial, } from "three";
 import { Nuclide } from "./Nuclide";
 import Spacer from "@/components/Spacer";
 import Segmented from "@/components/Segmented";
-
 const Configurator: React.FC = () => {
 	return (
 		<>
 			<Spacer />
 			<Segmented options={['M5', 'Nuclide', 'Viper']} onChange={(e) => alert(e)} />
-
-			{/* <div className="w-full h-fit">
+			{<div className="w-full h-fit">
 				<div style={{ width: "100%", height: "100vh " }} className="items-center align-middle justify-between">
-					<Canvas shadows camera={{ position: [6, 0, 15], fov: 30 }}>
+					<Canvas shadows camera={{position: [0, 0, 4.5], fov: 30 }}>
 						<spotLight position={[0, 15, 0]} angle={0.3} penumbra={1} castShadow intensity={2} shadow-bias={-0.0001} />
-						<ambientLight intensity={0.5} />
 						<PresentationControls global polar={[0.1, Math.PI / 30]} >
-							<Stage shadows={false}>
+							<Stage shadows={{type: "accumulative",color:"#D3D3D3", colorBlend:0.35,opacity:1}}>
 								<Nuclide />
 							</Stage>
-							<AccumulativeShadows position={[0, -0.5, 0]} frames={100} alphaTest={0.9} scale={10}>
-								<RandomizedLight amount={8} radius={10} ambient={1.5} position={[1, 5, -1]} />
-							</AccumulativeShadows>
 						</PresentationControls>
 					</Canvas>
 				</div>
-			</div> */}
+			</div>}
 		</>
 	);
 };
