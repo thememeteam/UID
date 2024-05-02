b1c071e82bd72bc504eb0573b1a6bf517d198c02
Rishblol <rish90093@gmail.com>
2024-05-02 13:11:36 +0530

    vaibhav short

--- src/app/configurator/page.tsx
+++ src/app/configurator/page.tsx
@@ -4,7 +4,7 @@ import Spacer from "@/components/Spacer";
 import NuclideModel from "@/components/models/Nuclide";
 import SLSModel from "@/components/models/SLS";
 import ViperModel from "@/components/models/Viper";
-import { PresentationControls, Stage } from "@react-three/drei";
+import { Environment, MeshReflectorMaterial, PresentationControls, Sky, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 
@@ -22,11 +22,12 @@ const Configurator: React.FC = () => {
 			<div className="row-start-2 col-start-1">
 				<Canvas shadows camera={{ position: [0, 0, 4.5], fov: 30 }}>
 					<spotLight
-						position={[0, 15, 0]}
+						position={[-4, 5, 0]}
 						angle={0.3}
 						penumbra={1}
 						castShadow
-						intensity={2}
+						receiveShadow
+						intensity={3}
 						shadow-bias={-0.0001}
 					/>
 					<PresentationControls global>
@@ -35,7 +36,7 @@ const Configurator: React.FC = () => {
 								type: "accumulative",
 								color: "#D3D3D3",
 								colorBlend: 0.35,
-								opacity: 1,
+								opacity: 2,
 							}}
 						>
 							{/* don't do conditional rendering for better performance */}
@@ -44,6 +45,23 @@ const Configurator: React.FC = () => {
 							<SLSModel visible={current === "SLS"} />
 						</Stage>
 					</PresentationControls>
+					<mesh rotation={[-Math.PI / 2, 0, 0]}>
+						<planeGeometry args={[50, 50]} />
+						<MeshReflectorMaterial
+							blur={[300, 100]}
+							resolution={2048}
+							mixBlur={1}
+							mixStrength={80}
+							roughness={1}
+							depthScale={1.2}
+							minDepthThreshold={0.4}
+							maxDepthThreshold={1.4}
+							color="#050505"
+							metalness={0.5}
+							mirror={0}
+						/>
+					</mesh>
+					<Environment preset="city"/>
 				</Canvas>
 			</div>
 			<div className="row-start-2 col-start-2 relative m-8 border border-card-stroke wedge">
