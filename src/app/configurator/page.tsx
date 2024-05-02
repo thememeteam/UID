390fa2a611a44d46e6ffe5b23c45d9594fa1e9c0
Rishblol <rish90093@gmail.com>
2024-05-02 13:44:24 +0530

    merry noel

--- src/app/configurator/page.tsx
+++ src/app/configurator/page.tsx
@@ -4,13 +4,11 @@ import Spacer from "@/components/Spacer";
 import NuclideModel from "@/components/models/Nuclide";
 import SLSModel from "@/components/models/SLS";
 import ViperModel from "@/components/models/Viper";
-import { Environment, MeshReflectorMaterial, PresentationControls, Sky, Stage } from "@react-three/drei";
+import { ContactShadows, Environment, MeshReflectorMaterial, PresentationControls, Sky, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
-
 const options = ["SLS", "Nuclide", "Viper"] as const;
 type CarOptions = (typeof options)[number];
-
 const Configurator: React.FC = () => {
 	const [current, setCurrent] = useState<CarOptions>("SLS");
 
@@ -21,47 +19,36 @@ const Configurator: React.FC = () => {
 			</div>
 			<div className="row-start-2 col-start-1">
 				<Canvas shadows camera={{ position: [0, 0, 4.5], fov: 30 }}>
-					<spotLight
-						position={[-4, 5, 0]}
-						angle={0.3}
-						penumbra={1}
-						castShadow
-						receiveShadow
-						intensity={3}
-						shadow-bias={-0.0001}
-					/>
 					<PresentationControls global>
+						<directionalLight position={[0,5,0]} intensity={2} />
 						<Stage
-							shadows={{
-								type: "accumulative",
-								color: "#D3D3D3",
-								colorBlend: 0.35,
-								opacity: 2,
-							}}
 						>
 							{/* don't do conditional rendering for better performance */}
-							<NuclideModel visible={current === "Nuclide"} />
-							<ViperModel visible={current === "Viper"} />
-							<SLSModel visible={current === "SLS"} />
+							<group>
+
+								<NuclideModel visible={current === "Nuclide"} />
+								<ViperModel visible={current === "Viper"} />
+								<SLSModel visible={current === "SLS"} />
+								<mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 200, 0]} scale={50}>
+									<planeGeometry />
+									<MeshReflectorMaterial
+										blur={[300, 100]}
+										resolution={2048}
+										mixBlur={1}
+										mixStrength={80}
+										roughness={1}
+										depthScale={1.2}
+										minDepthThreshold={0.4}
+										maxDepthThreshold={1.4}
+										color="#050505"
+										metalness={0.5}
+										mirror={0}
+									/>
+								</mesh>
+							</group>
 						</Stage>
 					</PresentationControls>
-					<mesh rotation={[-Math.PI / 2, 0, 0]}>
-						<planeGeometry args={[50, 50]} />
-						<MeshReflectorMaterial
-							blur={[300, 100]}
-							resolution={2048}
-							mixBlur={1}
-							mixStrength={80}
-							roughness={1}
-							depthScale={1.2}
-							minDepthThreshold={0.4}
-							maxDepthThreshold={1.4}
-							color="#050505"
-							metalness={0.5}
-							mirror={0}
-						/>
-					</mesh>
-					<Environment preset="city"/>
+					<Environment preset="warehouse"/>
 				</Canvas>
 			</div>
 			<div className="row-start-2 col-start-2 relative m-8 border border-card-stroke wedge">
