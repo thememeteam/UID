31dd42285188994cbe86a063ec48a55b3e8dadbd
Rishblol <rish90093@gmail.com>
2024-05-05 11:59:33 +0530

    minor changes

--- src/app/configurator/page.tsx
+++ src/app/configurator/page.tsx
@@ -4,7 +4,7 @@ import Spacer from "@/components/Spacer";
 import NuclideModel from "@/components/models/Nuclide";
 import SLSModel from "@/components/models/SLS";
 import ViperModel from "@/components/models/Viper";
-import { ContactShadows, Environment, MeshReflectorMaterial, PresentationControls, Sky, Stage } from "@react-three/drei";
+import { ContactShadows, Environment, MeshReflectorMaterial, PresentationControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 const options = ["SLS", "Nuclide", "Viper"] as const;
@@ -18,37 +18,20 @@ const Configurator: React.FC = () => {
 				<Spacer />
 			</div>
 			<div className="row-start-2 col-start-1">
-				<Canvas shadows camera={{ position: [0, 0, 4.5], fov: 30 }}>
-					<PresentationControls global>
-						<directionalLight position={[0,5,0]} intensity={2} />
+				<Canvas shadows camera={{ position: [0, 0.2, 4.5], fov: 30 }}>
+						<directionalLight position={[0, 5, 0]} intensity={2} />
+						<PresentationControls>
 						<Stage
 						>
 							{/* don't do conditional rendering for better performance */}
 							<group>
-
 								<NuclideModel visible={current === "Nuclide"} />
 								<ViperModel visible={current === "Viper"} />
 								<SLSModel visible={current === "SLS"} />
-								<mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 200, 0]} scale={50}>
-									<planeGeometry />
-									<MeshReflectorMaterial
-										blur={[300, 100]}
-										resolution={2048}
-										mixBlur={1}
-										mixStrength={80}
-										roughness={1}
-										depthScale={1.2}
-										minDepthThreshold={0.4}
-										maxDepthThreshold={1.4}
-										color="#050505"
-										metalness={0.5}
-										mirror={0}
-									/>
-								</mesh>
 							</group>
 						</Stage>
-					</PresentationControls>
-					<Environment preset="warehouse"/>
+						</PresentationControls>
+						
 				</Canvas>
 			</div>
 			<div className="row-start-2 col-start-2 relative m-8 border border-card-stroke wedge">
