4dc510d24b58c2829cad39518f8dd9c81235a13c
J S Vaibhav <151757419+Agerarsr@users.noreply.github.com>
2024-05-17 22:42:08 +0530

    added presets

--- src/app/configurator/presets.ts
+++ src/app/configurator/presets.ts
@@ -1,3 +1,4 @@
+import { redirect } from "next/dist/server/api-utils";
 import { Color, DoubleSide } from "three";
 import { metalness, roughness } from "three/examples/jsm/nodes/Nodes.js";
 
@@ -21,6 +22,25 @@ export const presets = {
         },
         interiorAccent: {
             color: new Color(ACCENT)
+        }
+    },
+    red: {
+        body: {
+            color: new Color(0xc22424),
+            roughness: 0,
+            metalness: 0.33
+        },
+        rims: {
+            color: new Color(0x000000)
+        },
+        calipers: {
+            color: new Color(0x000000)
+        },
+        interiorBase: {
+            color: new Color(0x000000)
+        },
+        interiorAccent: {
+            color: new Color(0xdadada)
         }
     },
     m5cs: {
@@ -59,6 +79,44 @@ export const presets = {
         },
         interiorAccent: {
             color: new Color(ACCENT)
+        }
+    },
+    blue: {
+        body: {
+            color: new Color(0x20337e),
+            roughness: 0,
+            metalness: 0.25
+        },
+        rims: {
+            color: new Color(0x8f8f8f)
+        },
+        calipers: {
+            color: new Color(0x000000)
+        },
+        interiorBase: {
+            color: new Color("white")
+        },
+        interiorAccent: {
+            color: new Color(0x7d7d7d)
+        }
+    },
+    pumpkin: {
+        body: {
+            color: new Color(0xa55c18),
+            roughness: 0,
+            metalness: 0.20
+        },
+        rims: {
+            color: new Color(0x4f4f4f)
+        },
+        calipers: {
+            color: new Color(0xa55c18)
+        },
+        interiorBase: {
+            color: new Color(0x4f4f4f)
+        },
+        interiorAccent: {
+            color: new Color(0x4f4f4f)
         }
     }
 };
